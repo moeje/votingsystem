4e171612f251d3d80a215879eb43f019a22b6a70
moeje <moe.je@web.de>
2013-01-30 19:30:25 +0100

    dingens

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,3 +1,7 @@
 ActiveAdmin.register Category do
-  
+   index do
+    column :name
+    default_actions
+   
+  end
 end
--- app/admin/poll_types.rb
+++ app/admin/poll_types.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register PollType do
+
+ index do
+    column :name
+    default_actions
+   
+  end
   
 end
