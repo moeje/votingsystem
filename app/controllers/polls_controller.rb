1cd15c8c20e8ef2a1726c31ebe7a39b37bc39f84
moeje <moe.je@web.de>
2013-01-30 16:15:10 +0100

    bla

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -61,12 +61,14 @@ class PollsController < ApplicationController
 	def create
 	  @poll = Poll.new(params[:poll])
 	  @poll.creator_id = current_creator.id	
+    @poll.shorturl = "#{request.protocol}#{request.host_with_port}/poll_voting/vote/#{@poll.slug}"
 
 	  respond_to do |format|
 	    if @poll.save
 
-      @poll.shorturl = tinyfy("#{request.protocol}#{request.host_with_port}/poll_voting/vote/#{@poll.slug}")
-      @poll.save
+      #@poll.shorturl = tinyfy("#{request.protocol}#{request.host_with_port}/poll_voting/vote/#{@poll.slug}")
+      
+      #@poll.save
 			answer_possibilities = params[:poll][:answer_possibilities_attributes]
 			answer_possibilities.each do |k,v|
 				v.each do |k1,v1|
